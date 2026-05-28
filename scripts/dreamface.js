07be573dd9f754272e4bdb5eb4af042cc55bf65f
Curtin <curtinp118@gmail.com>
2026-05-28 15:45:56 +0800

    refactor(dreamface): var→const, 修复缩进混乱, 移除注释代码

--- scripts/dreamface.js
+++ scripts/dreamface.js
@@ -1,6 +1,7 @@
 /******************************
-脚本功能：dreamface 3.0.0-解锁会员
+脚本功能：Dreamface 3.0.0-解锁会员
 更新时间：2024-06-08
+作者：Curtinp118
 *******************************
 [rewrite_local]
 https://www.dreamfaceapp.com/df-server/user/save_user_login url script-response-body https://raw.githubusercontent.com/curtinp118/QuantumultX/refs/heads/main/scripts/dreamface.js
@@ -8,30 +9,27 @@ https://www.dreamfaceapp.com/df-server/user/save_user_login url script-response-
 [mitm]
 hostname = www.dreamfaceapp.com
 *******************************/
-var objc = JSON.parse($response.body);
 
-    objc = {
-  "data": {
-    "rights": {
-      "expires_date_format": "2099-09-09 19:27:05.000",
-      "vip_type": "TRY_YEAR_PACKAGE",
-      "have_trial": false,
-      "vip_remainder_day": 9999,
-      "vip_label": true,
-      "expires_date": 4092595200000
+const body = {
+  data: {
+    rights: {
+      expires_date_format: "2099-09-09 19:27:05.000",
+      vip_type: "TRY_YEAR_PACKAGE",
+      have_trial: false,
+      vip_remainder_day: 9999,
+      vip_label: true,
+      expires_date: 4092595200000,
     },
-    "device_name": "iPhone13,3",
-    "system_version": "17.1.1",
-    "app_version": "3.0.0",
-    "app_package_name": "DreamFace",
-    //"user_id": "08A900C5-E40E-43D9-B3F1-D5118195DFA6",
-    "device_system": "iOS",
-    "country_code": "cn"
+    device_name: "iPhone13,3",
+    system_version: "17.1.1",
+    app_version: "3.0.0",
+    app_package_name: "DreamFace",
+    device_system: "iOS",
+    country_code: "cn",
   },
-  "status_code": "THS12140000000",
-  "extend": {
-  },
-  "status_msg": "Success"
-}
+  status_code: "THS12140000000",
+  extend: {},
+  status_msg: "Success",
+};
 
-$done({body : JSON.stringify(objc)});
+$done({ body: JSON.stringify(body) });
